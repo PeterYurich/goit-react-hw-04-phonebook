413c1847a1bf337f2b2d77e4fcaad29ba79cbd66
Peter <peteryurich@gmail.com>
2022-09-07 22:50:09 +0300

    task-2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import user from './alya-back-end/user.json';
 import Profile from './components/Profile/Profile';
 import data from './alya-back-end/data.json';
-import Statistics from './components/Statistics';
+import Statistics from './components/Statistics/Statistics';
 
 
 console.log(user)
@@ -18,7 +18,7 @@ function App() {
       location={user.location}
       avatar={user.avatar}
       stats={user.stats} />
-      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics title="Upload stats" stats={data} /> */}
       <Statistics stats={data} /></>
   )
 }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import css from "./Statistics.module.css";
-import propTypes, { arrayOf } from "prop-types";
+import propTypes from "prop-types";
+
+console.log(css);
 
 const Statistics = ({ title, stats }) => {
-  <section className={css.statistics}>
-    {title && <h2 className={css.title}>{title}</h2>}
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
 
-    <ul className={css.stat - list}>
-      {stats.map((item) => {
-        <li className={css.item}>
-          <span className={css.label}>{item.label}</span>
-          <span className={css.percentage}>{item.percentage}</span>
-        </li>;
-      })}
-    </ul>
-  </section>;
+      <ul className={css.stat_list}>
+        {stats.map(item => (
+          <li key={item.id} className={css.stat_item}>
+            <span className={css.label}>{item.label}</span>
+            <span className={css.percentage}>{item.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 };
 
 Statistics.propTypes = {
   title: propTypes.string,
-  stats: arrayOf(propTypes.object).isRequired,
+  stats: propTypes.arrayOf(propTypes.object).isRequired,
 };
+
+export default Statistics;
