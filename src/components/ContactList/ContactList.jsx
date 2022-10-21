0649a57955a7b7f234f1d6ec2a535586bab82bf1
Peter <peteryurich@gmail.com>
2022-10-21 17:16:51 +0300

    o my God

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import css from '../../components/styles.module.scss';
+import css from 'components/styles.module.scss';
 
 const ContactList = ({ toRender, deleteContact }) => {
   // console.log(allContacts, toRender, deleteContact)
